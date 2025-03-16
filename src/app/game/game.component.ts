ea2a21c157e41e01fbe2e9e20b9fdedbb02a8b22
Mihael0599 <mihaeldraz10@gmail.com>
2025-03-16 17:20:46 +0100

    added rules animations and funktion to add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Game } from 'src/models/game';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
@@ -6,11 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
-pickCardAnimation = false;
-  constructor(){ }
+  pickCardAnimation = false;
+  game: Game;
+  currentCard: string = '';
 
 
-  pickCard(){
-    this.pickCardAnimation = true;
+  constructor(public dialog: MatDialog) { }
+
+
+  ngOnInit(): void {
+    this.newGame();
   }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  pickCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
+      this.game.currtenPlayer++;
+      this.game.currtenPlayer = this.game.currtenPlayer % this.game.players.length
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+      }, 1000)
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name); 
+      }
+    });
+  }
+
 }
